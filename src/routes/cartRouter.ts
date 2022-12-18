102fcbb35cddf27ae04933bf9595efb06ecad96f
Ashutosh Sharma <104469510+mr-ashu@users.noreply.github.com>
2022-12-19 03:30:56 +0530

    backaend

---
+++ src/routes/cartRouter.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { deleteCart, getCart, setCart,  } from "../controller/cart/cart";
+import { deleteDocterCart, getDocterCart, setDocterCart } from "../controller/cart/docterCart";
+
+export const cartRouter = express.Router();
+
+cartRouter.get("/",getCart);
+cartRouter.post("/",setCart);
+cartRouter.delete("/:id",deleteCart);
+
+cartRouter.get("/docter", getDocterCart)
+cartRouter.post("/docter", setDocterCart)
+cartRouter.delete("/docter",deleteDocterCart);
